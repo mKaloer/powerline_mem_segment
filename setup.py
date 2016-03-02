2e6293fa7de8e1b21f1aea35cc4e5b6a105958f8
Mads Kalør <mads@kaloer.com>
2016-03-02 12:55:21 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='powerline-mem-segment',
-      version='1.0',
+      version='2.0',
       description='Memory segment for Powerline',
       author='Mads Kaloer',
       author_email='mads@kaloer.com',
       packages=['powerlinemem'],
       url='https://github.com/mKaloer/powerline_mem_segment',
-      download_url='https://github.com/mKaloer/powerline_mem_segment/tarball/1.0',
+      download_url='https://github.com/mKaloer/powerline_mem_segment/tarball/2.0',
       install_requires=[
           "psutil"
       ]
