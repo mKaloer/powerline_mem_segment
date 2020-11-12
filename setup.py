797a8b3f49c5b77e91333a3d5c6bb58132bfbbd0
Mads Kalør <mads@kaloer.com>
2020-11-12 08:54:49 +0100

    Release v2.4
    Adds swap memory

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 
 setup(name='powerline-mem-segment',
-      version='2.3',
+      version='2.4',
       description='Memory segment for Powerline',
       author='Mads Kaloer',
       author_email='mads@kaloer.com',
       packages=['powerlinemem'],
       url='https://github.com/mKaloer/powerline_mem_segment',
-      download_url='https://github.com/mKaloer/powerline_mem_segment/tarball/2.1',
       install_requires=[
           "psutil"
       ]
